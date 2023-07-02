eb7567416ce901e6bff0ec00782a2b487dd3cf3e
Rajat Dixit <Rajatdixithbtu@gmail.com>
2023-07-02 22:08:42 +0530

    add

--- src/main/java/com/driver/DeliveryPartner.java
+++ src/main/java/com/driver/DeliveryPartner.java
@@ -5,9 +5,25 @@ public class DeliveryPartner {
     private String id;
     private int numberOfOrders;
 
+    public DeliveryPartner(String id, int numberOfOrders) {
+        this.id = id;
+        this.numberOfOrders = numberOfOrders;
+    }
+
+    public DeliveryPartner() {
+    }
+
     public DeliveryPartner(String id) {
         this.id = id;
         this.numberOfOrders = 0;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setNumberOfOrders(int numberOfOrders) {
+        this.numberOfOrders = numberOfOrders;
     }
 
     public String getId() {
@@ -20,5 +36,13 @@ public class DeliveryPartner {
 
     public void setNumberOfOrders(Integer numberOfOrders) {
         this.numberOfOrders = numberOfOrders;
+    }
+
+    @Override
+    public String toString() {
+        return "DeliveryPartner{" +
+                "id='" + id + '\'' +
+                ", numberOfOrders=" + numberOfOrders +
+                '}';
     }
 }
